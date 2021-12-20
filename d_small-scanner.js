85ba038df02a0638113b48b084417179b0aee14b
Drew Strickland <andrew.flagship@gmail.com>
2021-12-20 00:32:50 -0500

    Update d_small-scanner.js

--- d_small-scanner.js
+++ d_small-scanner.js
@@ -22,7 +22,7 @@ export async function main(ns) {
     });
     let pwnTarget = pwnable.shift();
     while(pwnTarget){
-      await ns.run()
+      await ns.run("t_pwn.js", 1, pwnTarget);
       pwnTarget = pwnable.shift();
       await ns.sleep(INTERNAL_INTERVAL_MS)
     }
